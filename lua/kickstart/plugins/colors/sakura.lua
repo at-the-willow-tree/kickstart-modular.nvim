156ae9fa6b5abe12b6a463789afbb7d8892a1041
Will Kent <will.kent@motivait.net>
2025-12-11 12:27:20 +0000

    add buffer visible colors

--- lua/kickstart/plugins/colors/sakura.lua
+++ lua/kickstart/plugins/colors/sakura.lua
@@ -15,12 +15,19 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     vim.cmd 'hi BufferDefaultCurrentERROR guibg=none'
     vim.cmd 'hi BufferDefaultInactive guibg=none guifg=#5a525b'
     vim.cmd 'hi BufferDefaultInactiveTarget guibg=none guifg=#d6c1c5'
-    vim.cmd 'hi BufferDefaultInactiveSign guibg=none guifg=#d6c1c5'
+    vim.cmd 'hi BufferDefaultInactiveSign guibg=none guifg=none'
     vim.cmd 'hi BufferDefaultInactiveMod guibg=none'
     vim.cmd 'hi BufferDefaultInactiveADDED guibg=none'
     vim.cmd 'hi BufferDefaultInactiveCHANGED guibg=none'
     vim.cmd 'hi BufferDefaultInactiveDELETED guibg=none'
     vim.cmd 'hi BufferDefaultInactiveERROR guibg=none'
+    vim.cmd 'hi BufferDefaultVisible guibg=none'
+    vim.cmd 'hi BufferDefaultVisibleTarget guibg=none guifg=#d6c1c5'
+    vim.cmd 'hi BufferDefaultVisibleMod guibg=none'
+    vim.cmd 'hi BufferDefaultVisibleADDED guibg=none'
+    vim.cmd 'hi BufferDefaultVisibleCHANGED guibg=none'
+    vim.cmd 'hi BufferDefaultVisibleDELETED guibg=none'
+    vim.cmd 'hi BufferDefaultVisibleERROR guibg=none'
   end),
   group = vim.api.nvim_create_augroup('Sakura Barbar', {}),
 })
