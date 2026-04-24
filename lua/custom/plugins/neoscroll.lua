b36f1b24b7f5e0c042cbb14083e9e2896c4b36cb
Will Kent <will.kent@motivait.net>
2026-04-24 09:44:43 +0100

    update

--- lua/custom/plugins/neoscroll.lua
+++ lua/custom/plugins/neoscroll.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    'karb94/neoscroll.nvim',
-    opts = {
-      mappings = {
-        '<C-u>',
-        '<C-d>',
-        '<C-b>',
-        '<C-f>',
-        '<C-y>',
-        '<C-e>',
-        'zt',
-        'zz',
-        'zb',
-      },
-      hide_cursor = true,
-      stop_eof = true,
-      duration_multiplier = 0.15,
-    },
-  },
+  -- {
+  --   'karb94/neoscroll.nvim',
+  --   opts = {
+  --     mappings = {
+  --       '<C-u>',
+  --       '<C-d>',
+  --       '<C-b>',
+  --       '<C-f>',
+  --       '<C-y>',
+  --       '<C-e>',
+  --       'zt',
+  --       'zz',
+  --       'zb',
+  --     },
+  --     hide_cursor = true,
+  --     stop_eof = true,
+  --     duration_multiplier = 0.15,
+  --   },
+  -- },
 }
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -1,6 +1,28 @@
 -- LSP Plugins
 return {
   {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    opts = {
+      preset = 'powerline',
+      options = {
+        add_messages = {
+          messages = true,
+          display_count = true,
+        },
+        multilines = {
+          enabled = true,
+          always_show = true,
+        },
+        show_source = {
+          enabled = true,
+          if_many = true,
+        },
+      },
+    },
+  },
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -163,32 +185,35 @@ return {
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
-        severity_sort = true,
-        float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN } },
-        signs = vim.g.have_nerd_font and {
-          text = {
-            [vim.diagnostic.severity.ERROR] = '󰅚 ',
-            [vim.diagnostic.severity.WARN] = '󰀪 ',
-            [vim.diagnostic.severity.INFO] = '󰋽 ',
-            [vim.diagnostic.severity.HINT] = '󰌶 ',
-          },
-        } or {},
-        virtual_text = {
-          current_line = false,
-          source = 'if_many',
-          spacing = 2,
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
-        },
+        virtual_text = false,
       }
+      -- vim.diagnostic.config  {
+      --   severity_sort = true,
+      --   float = { border = 'rounded', source = 'if_many' },
+      --   underline = { severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN } },
+      --   signs = vim.g.have_nerd_font and {
+      --     text = {
+      --       [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      --       [vim.diagnostic.severity.WARN] = '󰀪 ',
+      --       [vim.diagnostic.severity.INFO] = '󰋽 ',
+      --       [vim.diagnostic.severity.HINT] = '󰌶 ',
+      --     },
+      --   } or {},
+      --   virtual_text = {
+      --     current_line = false,
+      --     source = 'if_many',
+      --     spacing = 2,
+      --     format = function(diagnostic)
+      --       local diagnostic_message = {
+      --         [vim.diagnostic.severity.ERROR] = diagnostic.message,
+      --         [vim.diagnostic.severity.WARN] = diagnostic.message,
+      --         [vim.diagnostic.severity.INFO] = diagnostic.message,
+      --         [vim.diagnostic.severity.HINT] = diagnostic.message,
+      --       }
+      --       return diagnostic_message[diagnostic.severity]
+      --     end,
+      --   },
+      -- }
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
